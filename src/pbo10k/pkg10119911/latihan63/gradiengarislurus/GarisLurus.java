0859c54382833485db7cdc9f253b1938ad70d66d
gare15 <72866876+gare15@users.noreply.github.com>
2020-11-29 18:45:08 +0700

    Add files via upload

---
+++ src/pbo10k/pkg10119911/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119911.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author tegar
+ */ 
+public interface GarisLurus {
+    public int hitungGradien();
+}
